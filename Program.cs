96a7f2cd1967799b6e787c03b28dc4e32012c7e4
Gabriel Vega <gvega001@gmail.com>
2021-03-12 23:14:37 -0800

    selecting using toUpper

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace QueryGenericListCollection
 {
@@ -9,7 +10,57 @@ namespace QueryGenericListCollection
         {
            //populate a list of strings
            var items = new List<string>();
+            items.Add("AQua"); //add "aQua" to the end of the list
+            items.Add("RusT"); //add "RusT" to the end of the list
+            items.Add("yElLow"); // add "yElLow" to the end of the List 
+            items.Add("rEd"); //add "rEd" to the end of the list 
 
+            //display initial List
+            Console.WriteLine("items contains:");
+            foreach (var item in items)
+            {
+                Console.WriteLine(item); 
+            }
+
+            Console.WriteLine(); //output end of line
+
+            //convert to uppercase, select those starting with "R" and sort
+            var startsWithR =
+                from item in items
+                let uppercaseString = item.ToUpper()
+                where uppercaseString.StartsWith("R")
+                orderby uppercaseString
+                select uppercaseString;
+
+            //display query results
+            Console.WriteLine("results of query startsWithR");
+            foreach (var item in startsWithR)
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.WriteLine();// output end of the line
+
+           items.Add("rUby");
+           items.Add("SaFfRon");
+
+           //display initial List
+           Console.WriteLine("items contains");
+           foreach (var item in items)
+           {
+               Console.WriteLine(item);
+           }
+
+           Console.WriteLine(); //output end of line
+            
+           //display updated query results
+           Console.WriteLine("results of query startsWithR");
+           foreach (var item in startsWithR)
+           {
+               Console.WriteLine(item);
+           }
+
+           Console.WriteLine(); //output end of the line
         }
     }
 }
