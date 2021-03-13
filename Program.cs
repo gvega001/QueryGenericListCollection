8501ac8a435b342ff53f62f57c201bd185ee903c
Gabriel Vega <gvega001@gmail.com>
2021-03-12 20:23:57 -0800

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace QueryGenericListCollection
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           //populate a list of strings
+           var items = new List<string>();
+
+        }
+    }
+}
